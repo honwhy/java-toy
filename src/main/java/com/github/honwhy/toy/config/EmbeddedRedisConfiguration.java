c639fca36e0dada2d8b5709ed10562e4f9b8abcc
Honwhy Wang <honwhy.wang@gmail.com>
2020-08-12 01:18:51 +0800

    add listen ports setting

--- src/main/java/com/github/honwhy/toy/config/EmbeddedRedisConfiguration.java
+++ src/main/java/com/github/honwhy/toy/config/EmbeddedRedisConfiguration.java
@@ -1,5 +1,6 @@
 package com.github.honwhy.toy.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +11,8 @@ public class EmbeddedRedisConfiguration {
 
     @Bean
     @ConditionalOnExpression("#{systemProperties['spring.profiles.active'] == 'local' || systemProperties['spring.profiles.active'] == 'dev'}")
-    public RedisServer redisServer() {
-        RedisServer redisServer = new RedisServer();
+    public RedisServer redisServer(@Value("${spring.redis.port}")Integer port) {
+        RedisServer redisServer = new RedisServer(port);
         redisServer.start();
         return redisServer;
     }
--- src/main/java/com/github/honwhy/toy/config/EmbeddedZookeeperConfiguration.java
+++ src/main/java/com/github/honwhy/toy/config/EmbeddedZookeeperConfiguration.java
@@ -2,6 +2,7 @@ package com.github.honwhy.toy.config;
 
 import lombok.SneakyThrows;
 import org.apache.curator.test.TestingServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +14,8 @@ public class EmbeddedZookeeperConfiguration {
     @Bean
     //@ConditionalOnProperty(prefix="spring.profiles", name="active", havingValue = "dev")
     @ConditionalOnExpression("#{systemProperties['spring.profiles.active'] == 'local' || systemProperties['spring.profiles.active'] == 'dev'}")
-    public TestingServer testingServer() {
-        TestingServer testingServer = new TestingServer(2181, false);
+    public TestingServer testingServer(@Value("${spring.zookeeper.port}")Integer port) {
+        TestingServer testingServer = new TestingServer(port, false);
         testingServer.start();
         return testingServer;
     }
